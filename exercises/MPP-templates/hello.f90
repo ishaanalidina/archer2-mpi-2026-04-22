a43fa3c7f3679d2e869a1eb0cdb9bdaff06a96a3
Ishaan Alidina ARCHER 2 MPI Training Course <74453831+ishaanalidina@users.noreply.github.com>
2026-04-22 16:16:33 +0100

    An initial implementation using send and receive to evaluate the overall value of pi.

--- exercises/MPP-templates/hello.f90
+++ exercises/MPP-templates/hello.f90
@@ -6,7 +6,8 @@ program hello
 
   integer :: ierr, rank, size, len, namelen, i, N, startidx, endidx
   character*(MPI_MAX_PROCESSOR_NAME) :: procname
-  double precision :: pival, val
+  double precision :: pival, val, pival_temp
+  integer :: status(MPI_STATUS_SIZE)
 
   N = 840
   pival = 0.0
@@ -30,6 +31,22 @@ program hello
   pival = (4.0 / N) * pival
 
   write(*,*) 'Contribution to pi calculated from rank: ', rank, 'of machine: ', procname(1:namelen), " : ", pival
+
+if (size .gt. 1) then
+  if (rank .eq. 0) then
+        do i = 1, size - 1
+        pival_temp = pival
+        call MPI_Recv(pival, 1, MPI_DOUBLE_PRECISION, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, status, ierr)
+        pival = pival_temp + pival        
+        end do
+  else
+        call MPI_Ssend(pival, 1, MPI_DOUBLE_PRECISION, 0, 0, MPI_COMM_WORLD, ierr)
+  end if
+end if
+
+if (rank .eq. 0) then
+  write(*, *) "Evaluation of pi on rank 0: ", pival
+end if
 
   call MPI_Finalize(ierr)
 
